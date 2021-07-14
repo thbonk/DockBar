15e86d1c95c4b2169e339e93caec80a509dbc799
Thomas Bonk <thomas@meandmymac.de>
2021-07-14 18:43:42 +0200

    Initial Commit

---
+++ DockBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DockBar
+//
+//  Created by Thomas Bonk on 14.07.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
